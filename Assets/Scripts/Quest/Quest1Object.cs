a98b4631d594f55ee883effd74f448535638c59e
hojun999 <148923751+999devel@users.noreply.github.com>
2024-03-20 23:54:12 +0900

    물음표/느낌표 오브젝트 할당
    물음표/느낌표 오브젝트 할당

--- Assets/Scripts/Quest/Quest1Object.cs
+++ Assets/Scripts/Quest/Quest1Object.cs
@@ -36,6 +36,7 @@ public class Quest1Object : QuestObject
         {
             questManager.MoveToNextQuest();
             SetNextDialogueID();
+            ActiveQuestionMarkOnClear();
         }
     }
 
